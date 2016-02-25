a8b0f1741065f3ea882fbbb53948df620e646b0e
Harshada Kurahatti <harshada.kurahatti@gmail.com>
2016-02-24 21:23:37 -0800

    Added the logo image.

--- js/app.js
+++ js/app.js
@@ -1,22 +1,24 @@
-var app = angular.module('instagramModule');
+var app = angular.module('instagramModule', []);
 app.controller('myCtrl', function ($scope, $http, $timeout) {
-    $scope.submit = function (tagName) {
+    $scope.submitTag = function (tagName) {
         $scope.tagName = tagName;
 
         var url = "https://api.instagram.com/v1/tags/" + tagName + "/media/recent";
         var request = {
             outputMode: 'json',
             callback: 'JSON_CALLBACK',
-            client_id: '35d594c5f644432c98eb1cbd8dacefb6'
+            client_id: '53541f77dbab454392d271e749fcb828'
         };
 
         $http({
-                method: 'JSON',
+                method: 'JSONP',
                 url: url,
                 params: request
             })
             .then(function (result) {
+                    console.log(result);
                     $scope.results = result.data.data;
+                    console.log(result.data);
                     if ($scope.results.length === 0) {
                         alert("There are no results with this tag! :(")
                     }
